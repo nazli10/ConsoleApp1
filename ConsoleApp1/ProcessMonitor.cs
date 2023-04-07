faff0651ce4a5c0839f60530b97cec0768e82e5a
nazli10 <yilnaz987@gmail.com>
2023-04-08 01:21:35 +0300

    Add project files.

---
+++ ConsoleApp1/ProcessMonitor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+public class ProcessMonitor
+{
+    public static void Main(string[] args)
+    {
+        KillProcess(args);
+    }
+
+    public static void KillProcess(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Usage: monitor.exe process_name max_lifetime monitoring_frequency");
+            return;
+        }
+
+        string processName = args[0];
+        int maxLifetime = int.Parse(args[1]);
+        int monitoringFrequency = int.Parse(args[2]);
+
+        Console.WriteLine($"Monitoring process {processName} with max lifetime {maxLifetime} and frequency {monitoringFrequency}");
+
+        while (true)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                foreach (Process process in processes)
+                {
+                    if ((DateTime.Now - process.StartTime).TotalMinutes > maxLifetime)
+                    {
+                        Console.WriteLine($"Process {process.ProcessName} with PID {process.Id} exceeded max lifetime and will be killed.");
+                        process.Kill();
+                    }
+                }
+            }
+
+            Thread.Sleep(monitoringFrequency * 60 * 1000);
+        }
+    }
+}
---
+++ ConsoleApp1Tests/ProcessMonitorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class PrgramMainTests
+    {
+        [TestMethod()]
+        public void TestProcessKilling()
+        {
+            // Start a long-running process
+            Process process = Process.Start("notepad.exe");
+
+            // Monitor the process with max lifetime of 1 minute and monitoring frequency of 1 second
+            string[] args = { "notepad", "1", "1" };
+            ProcessMonitor.KillProcess(args);
+            //Thread monitorThread = new Thread(() => Program.Main(args));
+            //monitorThread.Start();
+
+            // Wait for the process to be killed
+            process.WaitForExit();
+            //monitorThread.Abort();
+        }
+    }
+}
